d5163464a7a1e9b99f61e9e1e8395b7b2b30749b
Rajiv Kumar <mahirjv@gmail.com>
2023-09-13 08:59:43 +0530

    final project

---
+++ script.js
@@ -0,0 +1,66 @@
+// ABOUT PAGE
+$(function () {
+
+    $(".banner").fadeOut(1000);
+    $(".banner").fadeIn(2000);
+
+});
+
+// GALLERY PAGE
+
+
+$(".button").click(function () {
+    $(".gallery-table").fadeOut();
+});
+
+$(".button1").click(function () {
+    $(".gallery-table").fadeIn();
+});
+
+
+// VIDEO PAGE
+$(document).ready(function () {
+
+
+    $(".newbmw1").click(function () {
+        $(".new-bmw").fadeOut();
+    });
+    $(".newbmw2").click(function () {
+        $(".new-bmw").fadeIn();
+    });
+
+
+    $(".offroadbmw1").click(function () {
+        $(".offroad-bmw").fadeOut();
+    });
+    $(".offroadbmw2").click(function () {
+        $(".offroad-bmw").fadeIn();
+    });
+
+
+
+
+    $(".classicbmw1").click(function () {
+        $(".classic-bmw").fadeOut();
+    });
+    $(".classicbmw2").click(function () {
+        $(".classic-bmw").fadeIn();
+    });
+
+
+    $(".catchingeye1").click(function () {
+        $(".catching-eye").fadeOut();
+    });
+    $(".catchingeye2").click(function () {
+        $(".catching-eye").fadeIn();
+    });
+
+
+    $(".greenbmw1").click(function () {
+        $(".green-bmw").fadeOut();
+    });
+    $(".greenbmw2").click(function () {
+        $(".green-bmw").fadeIn();
+    });
+
+});
